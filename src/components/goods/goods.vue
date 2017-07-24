8069259ff70a76936a1eea6548b554afafc65e9d
yyr <570676207@qq.com>
2017-07-24 13:48:59 +0800

    first commit

---
+++ src/components/goods/goods.vue
@@ -0,0 +1,15 @@
+<template>
+
+</template>
+
+<script type="text/ecmascript-6">
+    export default {
+        data () {
+            return {};
+        }
+    };
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+
+</style>
